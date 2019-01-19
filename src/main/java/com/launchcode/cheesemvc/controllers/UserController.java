b0bea503e19088d9a92653015317b9d0f88a50c4
Dustin Peterson <dustinr.peterson@gmail.com>
2019-01-18 23:11:56 -0600

    User signup 2 completed

--- src/main/java/com/launchcode/cheesemvc/controllers/UserController.java
+++ src/main/java/com/launchcode/cheesemvc/controllers/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
+
 @Controller
 @RequestMapping(value="user")
 public class UserController {
@@ -20,7 +22,12 @@ public class UserController {
     }
 
     @RequestMapping(value="add", method = RequestMethod.POST)
-    public String add(Model model, @ModelAttribute User user, String verify) {
+    public String add(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
+
+        if(errors.hasErrors()) {
+            model.addAttribute(user);
+            return "user/add";
+        }
 
         if(user.getPassword().equals(verify)) {
             model.addAttribute("user", user);
--- src/main/java/com/launchcode/cheesemvc/models/User.java
+++ src/main/java/com/launchcode/cheesemvc/models/User.java
@@ -1,9 +1,21 @@
 package com.launchcode.cheesemvc.models;
 
+import org.hibernate.validator.constraints.Email;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
 
+    @NotNull
+    @Size(min=5, max=15)
     private String username;
+
+    @NotNull
+    @Size(min=6)
     private String password;
+
+    @Email
     private String email;
 
 
