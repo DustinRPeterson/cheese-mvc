4b0bdaf10dd5cfc831c14447ddf3013316ffb77c
Dustin Peterson <dustinr.peterson@gmail.com>
2019-01-18 21:36:58 -0600

    Added CheeseType enum and display

--- src/main/java/com/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/launchcode/cheesemvc/controllers/CheeseController.java
@@ -2,12 +2,16 @@ package com.launchcode.cheesemvc.controllers;
 
 import com.launchcode.cheesemvc.models.Cheese;
 import com.launchcode.cheesemvc.models.CheeseData;
+import com.launchcode.cheesemvc.models.CheeseType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -28,11 +32,20 @@ public class CheeseController {
     public String displayAddCheeseForm(Model model) {
 
         model.addAttribute("title", "Add Cheese");
+        model.addAttribute(new Cheese());
+        model.addAttribute("cheeseTypes", CheeseType.values());
         return "cheese/add";
     }
 
     @RequestMapping(value="add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@ModelAttribute Cheese newCheese) {
+    public String processAddCheeseForm(@ModelAttribute @Valid Cheese newCheese, Errors errors, Model model) {
+
+        if(errors.hasErrors()) {
+            model.addAttribute("title", "Add Cheese");
+            return "cheese/add";
+
+        }
+
         CheeseData.add(newCheese);
         return "redirect:";
     }
